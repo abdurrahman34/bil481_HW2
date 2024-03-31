43c44b7f2a7943066e8c339dd8b37a0ab7040fb1
abdurrahman34 <135560202+abdurrahman34@users.noreply.github.com>
2024-03-31 22:59:06 +0300

    Add files via upload
    TestUtil.java file is created and added to the repository.

---
+++ TestUtil.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class TestUtil {
+    private Util util;
+
+    @Before
+    public void setUp() {
+        util = new Util();
+    }
+
+    
+}

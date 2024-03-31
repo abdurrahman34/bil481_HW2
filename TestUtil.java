273e11663c7bd04487e7f37c89a66c3f875cc3a6
abdurrahman34 <135560202+abdurrahman34@users.noreply.github.com>
2024-03-31 23:00:30 +0300

    Add files via upload
    testEvenNumberOfArgsShouldReturnFalse method is added and works fine.

--- TestUtil.java
+++ TestUtil.java
@@ -20,4 +20,8 @@ public class TestUtil {
         assertFalse("Expected false when only one argument is passed", util.compute(2));
     }
 
-}
+    @Test
+    public void testEvenNumberOfArgsShouldReturnFalse() {
+        assertFalse("Expected false with an even number of arguments", util.compute(1, 2));
+    }
+  }
